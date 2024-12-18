9d1db30f34ad8878a47dd81bdaab2ea5f88bb483
Saqib-DevSecOp <saqibahmad778866@gmail.com>
2024-12-18 20:35:53 +0500

    design : project list page design done

--- src/website/urls.py
+++ src/website/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 from .views import (
-    HomeView, AboutView, ContactView, ServicesView, ServiceDetailsView, TeamView, BlogView
+    HomeView, AboutView, ContactView, ServicesView, ServiceDetailsView, TeamView, BlogView, ProjectListView
 )
+
 app_name = 'website'
-urlpatterns =[
+urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('about/', AboutView.as_view(), name='about'),
     path('contact/', ContactView.as_view(), name='contact'),
     path('services/', ServicesView.as_view(), name='services'),
     path('service-details/', ServiceDetailsView.as_view(), name='service-details'),
+    path('project-list/', ProjectListView.as_view(), name='project-list'),
     path('team/', TeamView.as_view(), name='team'),
     path('blog/', BlogView.as_view(), name='blog'),
     path('blog-details/', BlogView.as_view(), name='blog-details'),
--- src/website/views.py
+++ src/website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+
 # Create your views here.
 
 class HomeView(TemplateView):
@@ -11,6 +12,7 @@ class HomeView(TemplateView):
         context['title'] = 'Home'
         return context
 
+
 class AboutView(TemplateView):
     template_name = 'website/about.html'
 
@@ -18,6 +20,7 @@ class AboutView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['title'] = 'About'
         return context
+
 
 class ContactView(TemplateView):
     template_name = 'website/contact.html'
@@ -27,6 +30,7 @@ class ContactView(TemplateView):
         context['title'] = 'Contact'
         return context
 
+
 class ServicesView(TemplateView):
     template_name = 'website/service_list.html'
 
@@ -34,6 +38,7 @@ class ServicesView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Services'
         return context
+
 
 class ServiceDetailsView(TemplateView):
     template_name = 'website/service_detail.html'
@@ -43,6 +48,25 @@ class ServiceDetailsView(TemplateView):
         context['title'] = 'Service Details'
         return context
 
+
+class ProjectListView(TemplateView):
+    template_name = 'website/project_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Projects'
+        return context
+
+
+class ProjectDetailsView(TemplateView):
+    template_name = 'website/project_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Project Details'
+        return context
+
+
 class TeamView(TemplateView):
     template_name = 'website/team_list.html'
 
@@ -51,6 +75,7 @@ class TeamView(TemplateView):
         context['title'] = 'Team'
         return context
 
+
 class BlogView(TemplateView):
     template_name = 'website/blog_list.html'
 
@@ -58,6 +83,7 @@ class BlogView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Blog'
         return context
+
 
 class BlogDetailsView(TemplateView):
     template_name = 'website/blog_detail.html'
